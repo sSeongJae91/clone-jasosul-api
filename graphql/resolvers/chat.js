494355d51ead5897d7c5ac23f24ba082e444a004
sSeongjae91 <ben91429@gmail.com>
2021-04-08 16:43:42 +0900

    chat

--- graphql/resolvers/chat.js
+++ graphql/resolvers/chat.js
@@ -1,10 +1,29 @@
-const chat = require('../../models/Chat');
+const Chat = require('../../models/Chat');
+const checkAuth = require('../../util/checkAuth');
 
 module.exports = {
     Query: {
 
     },
     Mutation: {
-        
+        async postMessage (_, content, context, info) {
+
+            if(content.length < 1) {
+                return Error("Message is Empty");
+            }
+
+            const user = checkAuth(context);
+
+            const message = new Chat({
+                body: content,
+                username: user.username,
+                createdAt: new Date(),
+                user: user.id
+            });
+
+            const sendMsg = message.save();
+
+            return sendMsg;
+        }
     }
 }
